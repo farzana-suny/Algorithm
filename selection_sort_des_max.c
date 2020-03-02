765e55c81a7f51eb3b8f7d13b31f1e793e28a2c0
farzana-suny <59108163+farzana-suny@users.noreply.github.com>
2020-03-01 23:27:58 -0800

    Add files via upload

---
+++ selection_sort_des_max.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+void selection_sort(int A[],int n);
+
+
+void selection_sort(int A[],int n)
+{
+	int i,max,j,temp;
+	for(i=0;i<n;i++)
+	{
+		max=i;
+		for(j=i+1;j<n;j++)
+		{
+		 if(A[j]>A[max])
+		 {
+		 	max=j;
+		 }
+		}
+		temp=A[i];
+		A[i]=A[max];
+		A[max]=temp;
+	}
+
+}
+int main()
+{
+	int n,i;
+	printf("enter the array size\n");
+	scanf("%d",&n);
+	int a[n];
+	printf("enter the array elements\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	//printf("display the array elements in unsorted order\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+	selection_sort(a,n);
+	printf("display the array elements in sorted order\n");
+	for(i=0;i<n;i++)
+	{
+		printf(" %d ",a[i]);
+	}
+
+}
+
